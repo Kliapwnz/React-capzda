16abe993c35eac64589e236085b7a3c6fba9d710
Evgeniy Klimovch <kliasanik@gmail.com>
2023-12-23 15:24:46 +0300

    Components start

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,35 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
+}
 
+function Rating() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    )
+}
+
+let Accordion = () => {
+    return <div>
+        <h3>Menu</h3>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     </div>
-  );
 }
 
 export default App;
