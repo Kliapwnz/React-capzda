760b63fa0073e2e90321fb60d1c58260849d2dfd
Evgeniy Klimovch <kliasanik@gmail.com>
2023-12-24 14:40:20 +0300

    props components

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 
+type RatingPropsType = {
+    selected: boolean
+}
+
 export function Rating() {
     return (
         <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
         </div>
     )
 }
 
-function Star() {
-    return <span>star </span>
+function Star(props: RatingPropsType) {
+    if (props.selected) {
+        return <span><b>star </b></span>
+    } else {
+        return <span>star </span>
+    }
 }
