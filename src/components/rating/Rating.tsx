37a08ebfc5d7429dd4eefdb6124be0c1daacd0a4
Evgeniy Klimovch <kliasanik@gmail.com>
2023-12-25 16:42:17 +0300

    uncontrolled components

--- src/components/rating/Rating.tsx
+++ src/components/rating/Rating.tsx
@@ -2,26 +2,26 @@ import React, {useState} from "react";
 
 
 type RatingType ={
-    value:number
+
 }
 type StatType ={
-    value:number
+    selected:boolean
 }
 
 export function Rating(props:RatingType) {
-let [value, setValue] = useState(0 | 1 | 2 | 3 | 4 | 5)
+let [value, setValue] = useState(0)
     return <div>
-        <Star /><button onClick={()=>{setValue(1)}}>1</button>
-        <Star /><button onClick={()=>{setValue(2)}}>2</button>
-        <Star /><button onClick={()=>{setValue(3)}}>3</button>
-        <Star /><button onClick={()=>{setValue(4)}}>4</button>
-        <Star /><button onClick={()=>{setValue(5)}}>5</button>
+        <Star selected={value > 0}/><button onClick={()=>{setValue(1)}}>1</button>
+        <Star selected={value > 1}/><button onClick={()=>{setValue(2)}}>2</button>
+        <Star selected={value > 2}/><button onClick={()=>{setValue(3)}}>3</button>
+        <Star selected={value > 3}/><button onClick={()=>{setValue(4)}}>4</button>
+        <Star selected={value > 4}/><button onClick={()=>{setValue(5)}}>5</button>
     </div>
 
 }
 
 function Star(props:StatType) {
-    if (props.value) {
+    if (props.selected) {
         return <span><b>☻ </b></span>
     } else {
         return <span>☺ </span>
