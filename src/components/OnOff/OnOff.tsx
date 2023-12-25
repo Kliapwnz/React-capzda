28ba127073b836c9b6136c674715733c126b0011
Evgeniy Klimovch <kliasanik@gmail.com>
2023-12-25 14:35:24 +0300

    useState process....

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -9,16 +9,20 @@ export let OnOff = (props: OnOffType) => {
     let [on, setOn] = useState(false);
 
     const onStyle = {
-        backgroundColor: on ? "green" : "silver"
+        backgroundColor: on ? "green" : "silver",
+        width: "50px",
+        height: "50px"
     }
     const offStyle = {
-        backgroundColor: on ? "silver" : "red"
+        backgroundColor: on ? "silver" : "red",
+        width: "50px",
+        height: "50px"
     }
 
     const indicatorStyle = {
-        width: "15px",
-        height: "15px",
-        borderRadius: "7px",
+        width: "30px",
+        height: "30px",
+        borderRadius: "15px",
         border: "1px solid black",
         display: "inline-block",
         marginLeft: "15px",
