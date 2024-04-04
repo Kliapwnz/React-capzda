b09fc0c62d822902f71b495137500a0300feb739
Evgeniy Klimovch <kliasanik@gmail.com>
2024-04-05 00:53:07 +0300

    storybook Accordion.stories.tsx

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,35 +1,35 @@
 import React from "react";
 
 type AccordionType = {
-    titleValue: string
-    collapsed: boolean
-    onChange: () => void
+   titleValue: string
+   collapsed: boolean
+   onChange: () => void
 }
 type AccordionTitleType = {
-    title: string
-    onClick: () => void
+   title: string
+   onClick: () => void
 }
 
 
 export let Accordion = (props: AccordionType) => {
-    return <div>
-        <AccordionTitle title={props.titleValue}
-                        onClick={props.onChange}
-        />
-        {!props.collapsed && <AccordionBody/>}
-    </div>
+   return <div>
+      <AccordionTitle title={props.titleValue}
+                      onClick={props.onChange}
+      />
+      {!props.collapsed && <AccordionBody/>}
+   </div>
 }
 
 
 let AccordionTitle = (props: AccordionTitleType) => {
-    return (
-        <h3 onClick={props.onClick}>{props.title}</h3>
-    )
+   return (
+      <h3 onClick={props.onClick}>{props.title}</h3>
+   )
 }
 let AccordionBody = () => {
-    return <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-    </ul>
+   return <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+   </ul>
 }
--- src/components/OnOff/OnOff.stories.tsx
+++ src/components/OnOff/OnOff.stories.tsx
@@ -4,14 +4,14 @@ import {action} from "@storybook/addon-actions";
 
 
 export default {
-    title: "OnOff",
-    component: OnOff,
+   title: "OnOff",
+   component: OnOff,
 }
-const callBack= action("clicked")
+const callBack = action("clicked")
 
 export const OnMode = () => <OnOff value={true} onClick={callBack}/>
 export const OffMode = () => <OnOff value={false} onClick={callBack}/>
 export const ModeChanging = () => {
-    const [value,setValue]= useState<boolean>(false)
-    return <OnOff value={value} onClick={setValue}/>
+   const [value, setValue] = useState<boolean>(false)
+   return <OnOff value={value} onClick={setValue}/>
 }
