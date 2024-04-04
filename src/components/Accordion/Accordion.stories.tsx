bae51ab32340aae9c50bf19e04fec7c9ab38c61b
Evgeniy Klimovch <kliasanik@gmail.com>
2024-04-05 01:04:44 +0300

    add onChange

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -4,44 +4,46 @@ import {Accordion} from './Accordion';
 import React, {useState} from "react";
 
 const meta: Meta<typeof Accordion> = {
-    component: Accordion,
+   component: Accordion,
 };
 
 export default meta;
 type Story = StoryObj<typeof Accordion>
 
 export const FirstStory: Story = {
-    args: {
-        titleValue: "™Berserk☺ ",
-        collapsed: true,
-    }
+   args: {
+      titleValue: "Hello",
+      collapsed: true,
+      onChange: () => {
+      }
+   }
 }
 
 export const ChangingAccordion = () => {
-    const [value, setValue] = useState<boolean>(true)
-    return <Accordion titleValue="™Berserk☺ " collapsed={value} onChange={()=>setValue(!value)}/>
+   const [value, setValue] = useState<boolean>(true)
+   return <Accordion titleValue="™Berserk☺ " collapsed={value} onChange={() => setValue(!value)}/>
 }
 
 export const CollapsedAccordion = () => {
-    return <Accordion titleValue="Accordion Collapsed"
-                      collapsed={true}
-                      onChange={() => {
-                      }}
-    />
+   return <Accordion titleValue="Accordion Collapsed"
+                     collapsed={true}
+                     onChange={() => {
+                     }}
+   />
 }
 export const OpenedAccordion = () => {
-    return <Accordion titleValue="Accordion Opened"
-                      collapsed={false}
-                      onChange={() => {
-                      }}
-    />
+   return <Accordion titleValue="Accordion Opened"
+                     collapsed={false}
+                     onChange={() => {
+                     }}
+   />
 }
-export const Accordion1 = () => {
-    const [collapsed, setCollapsed] = useState(false)
-    return <Accordion titleValue="Accordion"
-                      collapsed={collapsed}
-                      onChange={() => {
-                          setCollapsed(!collapsed)
-                      }}
-    />
+export const AccordionDemo = () => {
+   const [collapsed, setCollapsed] = useState(false)
+   return <Accordion titleValue="Accordion"
+                     collapsed={collapsed}
+                     onChange={() => {
+                        setCollapsed(!collapsed)
+                     }}
+   />
 }
