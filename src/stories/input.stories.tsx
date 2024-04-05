777f2b36361dfbcfc315258060d7ab7d291efb15
Evgeniy Klimovch <kliasanik@gmail.com>
2024-04-05 12:52:53 +0300

    ControlledSelect

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -23,6 +23,18 @@ export const ControlledCheckbox = () => {
    return <input type={"checkbox"} checked={parentValue} onChange={onChangeHandler}/>
 }
 export const ControlledSelect = () => {
+   const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+
+   const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+      setParentValue(e.currentTarget.value)
+   }
+
+   return <select value={parentValue} onChange={onChangeHandler}>
+      <option>none</option>
+      <option value={"1"}>Minks</option>
+      <option value={"2"}>Basel</option>
+      <option value={"3"}>Manchester</option>
+   </select>
 
 }
 
