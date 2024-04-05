bba73037e319fb2af305040c9a1aa5c5a3b9e8d3
Evgeniy Klimovch <kliasanik@gmail.com>
2024-04-05 12:29:21 +0300

    input.stories.tsx ControlledInput

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,9 +1,13 @@
+import {useState} from "react";
+import {action} from "@storybook/addon-actions";
+
 export default {
    title: "input"
 };
 
 export const ControlledInput = () => {
-
+   const [parentValue, setParentValue] = useState("")
+   return <input value={parentValue} onChange={action("want to change")}/>
 }
 export const ControlledCheckbox = () => {
 
